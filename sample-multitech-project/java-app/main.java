325c5fdc17f8aa2aac7982ffd8d617b5df44baa6
nidhihk003 <shpnidhi03@gmail.com>
2025-12-04 12:49:31 +0530

    Create main.java

---
+++ sample-multitech-project/java-app/main.java
@@ -0,0 +1,11 @@
+package com.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(addNumbers(5, 10));
+    }
+
+    public static int addNumbers(int a, int b) {
+        return a + b;
+    }
+}
