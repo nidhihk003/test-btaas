8ce4a59b163c61fd398ed03290b12cd74d9e1e96
nidhihk003 <shpnidhi03@gmail.com>
2026-02-05 10:46:12 +0530

    Update user.component.ts

--- sample-multitech-project/angular-app/user.component.ts
+++ sample-multitech-project/angular-app/user.component.ts
@@ -1,1 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { UserService } from './user.service';
 
+@Component({
+  selector: 'app-user',
+  template: `<div>{{user?.name}}</div>`
+})
+export class UserComponent implements OnInit {
+
+  user: any;
+
+  constructor(private userService: UserService) {}
+
+  ngOnInit() {
+    this.loadUser();
+  }
+
+  loadUser() {
+    this.userService.getUser(1).subscribe(res => {
+      this.user = res;
+    });
+  }
+}
