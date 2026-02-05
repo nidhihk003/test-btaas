be13287838f380181fb991972480c5778a0d1c58
nidhihk003 <shpnidhi03@gmail.com>
2026-02-05 10:52:04 +0530

    Update main.py

--- sample-multitech-project/python-app/main.py
+++ sample-multitech-project/python-app/main.py
@@ -1,2 +1,12 @@
-def hello_world():
-    print("Hello from commit API")
+from utils import validate_user
+
+def get_user(user_id: int):
+    user = {"id": user_id, "name": "Dummy Python User"}
+
+    if not validate_user(user):
+        raise Exception("Invalid user")
+
+    return user
+
+if __name__ == "__main__":
+    print(get_user(1))
