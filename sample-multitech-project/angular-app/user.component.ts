749e392fc69470822caba07fe4ed363a4698777c
nidhihk003 <shpnidhi03@gmail.com>
2026-02-05 15:34:41 +0530

    Update user.component.ts

--- sample-multitech-project/angular-app/user.component.ts
+++ sample-multitech-project/angular-app/user.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from './user.service';
+import { User } from '../models/user.model';
+
 
 @Component({
   selector: 'app-user',
